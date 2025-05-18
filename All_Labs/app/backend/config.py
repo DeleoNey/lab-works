ddeec921e0388e433c0197ec8d5a6fdbfaaa255c
DeleoNey <146377641+DeleoNey@users.noreply.github.com>
2025-05-18 18:08:43 +0300

    Update config.py

--- All_Labs/app/backend/config.py
+++ All_Labs/app/backend/config.py
@@ -4,7 +4,7 @@ import motor.motor_asyncio
 
 load_dotenv()
 
-GEO_DB_API_KEY="a514a370famsh4824cac8c5b598ap1277b0jsn300d354db274"
+GEO_DB_API_KEY="smth"
 MONGODB_HOST="localhost"
 MONGODB_PORT="27017"
 MONGO_DATABASE="city"
